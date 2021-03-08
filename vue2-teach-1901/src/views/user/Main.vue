7dcf8d765f54c6ee304ecc7d2ed67a4c51c9623e
huhuiyu <1069306849@qq.com>
2021-03-08 17:59:10 +0800

    1901班vue项目，登录，注册完成

---
+++ vue2-teach-1901/src/views/user/Main.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>用户首页</div>
+    <div> 欢迎：{{ userinfo.username }}({{ userinfo.nickname }}) </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'UserMain',
+  data() {
+    return {
+      userinfo: {}
+    };
+  },
+  methods: {
+    queryUserInfo() {
+      //查询用户登录信息
+      this.$ajax('/user/queryUserInfo', {}, function(data) {
+        console.log(data);
+        // 判断用户登录信息是否存在
+        this.userinfo = data.resultData.tbUser;
+        if (data.success && this.userinfo && this.userinfo.username) {
+          return;
+        }
+        // 没有就跳转到登录
+        this.$router.push('/user/login');
+      });
+    }
+  },
+  created() {
+    this.queryUserInfo();
+  }
+};
+</script>
