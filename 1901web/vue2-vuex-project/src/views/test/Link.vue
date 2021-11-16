31d67bef375f66df9fc7a0a90b062b104ca29307
huhuiyu <1069306849@qq.com>
2021-11-16 16:16:32 +0800

    map和递归级联

--- 1901web/vue2-vuex-project/src/views/test/Link.vue
+++ 1901web/vue2-vuex-project/src/views/test/Link.vue
@@ -6,6 +6,24 @@
       <button @click="jsJiecheng">计算阶乘</button>
       {{ numu }}-----{{ result1 }}------{{ result2 }}
     </div>
+    <hr />
+    <div>
+      <h1>递归完成未知级数的联动</h1>
+      <!-- <div>
+        {{ list }}
+      </div> -->
+      <div>
+        {{ aids }}
+        <span v-for="(datas, index) in list" :key="index">
+          <!-- {{ index }} -->
+          <select v-if="index <= maxLevel" v-model="aids[index]" @change="queryAreaByPid(aids[index], index + 2)">
+            <option v-for="d in datas" :key="d.aiid" :value="d.aid">
+              {{ d.name }}
+            </option>
+          </select>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -39,6 +57,14 @@ export default {
       numu: 1,
       result1: 1,
       result2: 1,
+      // 问题一，数组的数量不确定，所以是数组的数组
+      // 意思就是，如果联动是2级，那么这个list中有两个数组
+      // 分别对应一级和二级数据
+      list: [],
+      // 选中的值也是数组
+      aids: [null, null, null, null],
+      // 最大等级数
+      maxLevel: 0,
     }
   },
   methods: {
@@ -46,6 +72,46 @@ export default {
       this.result1 = jiecheng(this.numu)
       this.result2 = dgJc(this.numu)
     },
+    // 查询区域的递归方法，参数一是选择的地区编号
+    // 参数二是选中的地区编号是第几级的
+    queryAreaByPid(pid, level) {
+      console.log('递归查询区域的参数', pid, level)
+      let app = this
+      app.$ajax(
+        '/linkinfo/queryAreaByPid',
+        {
+          'tbAreaInfo.pid': pid,
+        },
+        function (data) {
+          let list = data.resultData.list
+          console.log('等级', level, '的数据', list)
+          // 递归停止条件，没有下一级数据就表示完成
+          if (!list || list.length == 0) {
+            // 确定最大等级
+            app.maxLevel = level - 2
+            console.log('停止的等级：', level, app.list)
+            // vue无法监听到数组数据变化，所以需要set方法告诉vue数据变化
+            for (let i = 0; i < app.list.length; i++) {
+              // 参数一，是通知数据变化的数组对象
+              // 参数二，数组中第几个数据变化
+              // 参数三，新值
+              app.$set(app.list, i, app.list[i])
+            }
+          } else {
+            // 添加等级数据到对应的数组位置
+            app.list[level - 1] = list
+            // 设置默认值
+            app.aids[level - 1] = list[0].aid
+            // 继续查询下一级（递归），默认都是查询第一个
+            app.queryAreaByPid(list[0].aid, level + 1)
+          }
+        }
+      )
+    },
+  },
+  created() {
+    // 启动一级的调用
+    this.queryAreaByPid(0, 1)
   },
 }
 </script>
--- 1902web/vue2-vuex-project/src/views/Map.vue
+++ 1902web/vue2-vuex-project/src/views/Map.vue
@@ -1,22 +1,51 @@
 <template>
   <div>
     <div>{{ title }}</div>
-
+    <hr />
+    <input type="text" v-model="num" />
+    <button @click="jisuan">阶乘</button>
+    {{ result1 }} ----- {{ result2 }}
+    <hr />
     <div>
       {{ plist }}
     </div>
   </div>
 </template>
 <script>
+// n的阶乘就是n*(n-1)...*1
+function jiecheng(n) {
+  let result = 1
+  for (let i = 1; i <= n; i++) {
+    result = result * i
+  }
+  return result
+}
+// 递归的要素，1：调用方法自身，2：递归必须要有终止的条件
+function dgjc(n) {
+  // 1的阶乘就是1
+  if (n == 1) {
+    return 1
+  }
+  // n的阶乘就是n*(n-1)阶乘
+  return n * dgjc(n - 1)
+}
+
 export default {
   name: 'Map',
   data() {
     return {
       title: 'Map相关知识',
       plist: [],
+      num: 1,
+      result1: 1,
+      result2: 1,
     }
   },
   methods: {
+    jisuan() {
+      this.result1 = jiecheng(this.num)
+      this.result2 = dgjc(this.num)
+    },
     query(pid) {
       this.$ajax(
         '/linkinfo/queryAreaByPid',
@@ -50,6 +79,19 @@ export default {
   },
   created() {
     this.query(0)
+    // map数据结构（练习set）
+    // List,Map,Set（输入一串字符串，统计出现了多少个字符，且出现的次数）
+    // a111bcc a,1 1,3 b,1 c,2
+    let mapInfo = new Map()
+    // map的特征就是键值对
+    mapInfo.set('one', '一，数字一')
+    mapInfo.set('love', '爱')
+    mapInfo.set('michael', '天使名，米迦勒')
+
+    console.log('map数据：', mapInfo)
+    // 取数据是通过key获取对应的值
+    console.log('one的翻译', mapInfo.get('one'))
+    // map添加相同的key是覆盖动作，也就是说key是无重复的
   },
 }
 </script>
